d62cb43b0830dfece5c1b126e26fbe50f5f9c7e8
zhaojq <zjq_joker@163.com>
2022-01-13 16:37:42 +0800

    feat: finish 03-diving-deeper-into-swiftui starter

---
+++ Raywenderlich-SwiftUI-by-Tutorials/03-diving-deeper-into-swiftui/README.md
@@ -0,0 +1,24 @@
+### [03-diving-deeper-into-swiftui](https://github.com/Coder-ZJQ/SwiftUI/commits/master/Raywenderlich-SwiftUI-by-Tutorials/03-diving-deeper-into-swiftui)
+
+|  Project  | Diff |
+| :-------: | :--: |
+|   start   |  \   |
+|  finish   |      |
+| challenge |      |
+
+
+
+- **Library**: **primitive views** & **modifiers**
+- **Neumorphism**: [新拟态](https://zhuanlan.zhihu.com/p/106071699)
+- [ButtonStyle](https://developer.apple.com/documentation/swiftui/buttonstyle/): 设置按钮样式（会使系统原本样式失效）
+- [Capsule](https://developer.apple.com/documentation/swiftui/Capsule/): 胶囊形状
+- [Group](https://developer.apple.com/documentation/swiftui/Group/): 将 View, Sence 等组成一组，利用 modifier 修饰 Group 会影响其中的所有成员
+- inset: 设置缩进
+- [previewLayout](https://developer.apple.com/documentation/swiftui/previewlayout/): 设置预览尺寸
+- [previewDevice](https://developer.apple.com/documentation/swiftui/previewdevice/): 设置模拟器预览设备
+- [preferredColorScheme](https://developer.apple.com/documentation/swiftui/anyview/preferredcolorscheme(_:)/): 设置颜色主题（暗黑模式）
+- font: 设置字体（会影响子 View）
+- GeometryReader: 适配屏幕尺寸
+  - GeometryProxy
+
+
--- Raywenderlich-SwiftUI-by-Tutorials/03-diving-deeper-into-swiftui/RGBullsEye/ViewExtension.swift
+++ Raywenderlich-SwiftUI-by-Tutorials/03-diving-deeper-into-swiftui/RGBullsEye/ViewExtension.swift
@@ -32,30 +32,39 @@
 
 import SwiftUI
 
-struct ColorCircle: View {
-  let rgb: RGB
-  let size: CGFloat
-
-  var body: some View {
-    ZStack {
-      Circle()
-        .fill(Color.element)
-        .northWestShadow()
-      Circle()
-        .fill(Color(red: rgb.red, green: rgb.green, blue: rgb.blue))
-        .padding(20)
-    }
-    .frame(width: size, height: size)
+extension View {
+  /// Simulate shining a light on the northwest edge of a view.
+  /// Light shadow on the northwest edge, dark shadow on the southeast edge.
+  ///   - parameters:
+  ///     - radius: The size of the shadow
+  ///     - offset: The value used for (-x, -y) and (x, y) offsets
+  func northWestShadow(
+    radius: CGFloat = 16,
+    offset: CGFloat = 6
+  ) -> some View {
+    return self
+      .shadow(
+        color: .highlight,
+        radius: radius,
+        x: -offset,
+        y: -offset)
+      .shadow(
+        color: .shadow, radius: radius, x: offset, y: offset)
   }
-}
 
-struct ColorCircle_Previews: PreviewProvider {
-  static var previews: some View {
-    ZStack {
-      Color.element
-      ColorCircle(rgb: RGB(), size: 200)
-    }
-    .frame(width: 300, height: 300)
-    .previewLayout(.sizeThatFits)
+  /// Simulate shining a light on the southeast edge of a view.
+  /// Light shadow on the southeast edge, dark shadow on the northwest edge.
+  ///   - parameters:
+  ///     - radius: The size of the shadow
+  ///     - offset: The value used for (-x, -y) and (x, y) offsets
+  func southEastShadow(
+    radius: CGFloat = 16,
+    offset: CGFloat = 6
+  ) -> some View {
+    return self
+      .shadow(
+        color: .shadow, radius: radius, x: -offset, y: -offset)
+      .shadow(
+        color: .highlight, radius: radius, x: offset, y: offset)
   }
 }
